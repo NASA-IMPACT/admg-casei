ab37de7cf112477f688baae079c81e7de1a619bc
Alice Rühl <alice@ds.io>
2020-05-05 15:52:55 +0200

    Add inpage nav to campaign template

--- cypress/e2e/campaign.test.js
+++ cypress/e2e/campaign.test.js
@@ -31,5 +31,20 @@ describe("Campaign", () => {
       })
 
     cy.get("main").find("header").find("[data-cy=overview-map]").should("exist")
+
+    cy.get("main").find("nav").should("exist")
+    cy.get("main")
+      .find("nav")
+      .find("a")
+      .should($anchor => {
+        expect($anchor, "5 items").to.have.length(5)
+        expect($anchor.eq(0), "first item").to.contain("Overview")
+        expect($anchor.eq(1), "first item").to.contain("Milestones")
+        expect($anchor.eq(2), "first item").to.contain(
+          "Platforms & Instruments"
+        )
+        expect($anchor.eq(3), "first item").to.contain("Data")
+        expect($anchor.eq(4), "first item").to.contain("Resources")
+      })
   })
 })
--- src/templates/campaign.js
+++ src/templates/campaign.js
@@ -4,33 +4,30 @@ import Layout from "../components/layout"
 
 import NorthAmerica from "../data/north-america.svg"
 
-const StatNumber = ({ number, label }) => (
+const StatNumber = ({ number = "--", label }) => (
   <>
-    <dd style={{ fontSize: `3rem` }}>{number}</dd>
+    <dd style={{ fontSize: `3rem` }}>{number.length === 0 ? "--" : number}</dd>
     <dt style={{ gridRowStart: 2, textTransform: `uppercase` }}>{label}</dt>
   </>
+)
+
+const InpageLink = props => (
+  <li style={{ margin: `0 1rem 0 0` }}>
+    <a href={props.to}>{props.children}</a>
+  </li>
 )
 
 const CampaignTemplate = ({ data: { campaign } }) => {
   return (
     <Layout>
-      <header
-        style={{
-          display: `flex`,
-        }}
-      >
+      <header style={{ display: `flex` }}>
         <div style={{ flex: `1.61803398875` }}>
           <div>
             <p>{campaign.shortname}</p>
             <h1>{campaign.longname}</h1>
             <p>{campaign.focus}</p>
           </div>
-          <dl
-            style={{
-              display: `grid`,
-            }}
-            data-cy="stats"
-          >
+          <dl style={{ display: `grid` }} data-cy="stats">
             <StatNumber
               number={campaign.countDeployments}
               label="Deployments"
@@ -46,6 +43,57 @@ const CampaignTemplate = ({ data: { campaign } }) => {
           <img src={NorthAmerica} alt="North America" data-cy="overview-map" />
         </div>
       </header>
+      <div
+        style={{
+          display: `flex`,
+          padding: `1rem`,
+          borderTop: `1px solid #9E9E9E`,
+          borderBottom: `1px solid #9E9E9E`,
+          justifyContent: `space-between`,
+          alignItems: `center`,
+        }}
+      >
+        <nav role="inpage-scroll">
+          <ul
+            style={{
+              display: `flex`,
+              flexDirection: `row`,
+              justifyContent: `flex-start`,
+              margin: 0,
+              listStyle: `none`,
+            }}
+          >
+            <InpageLink to="#overview">Overview</InpageLink>
+            <InpageLink to="#milestones">Milestones</InpageLink>
+            <InpageLink to="#platforms">Platforms & Instruments</InpageLink>
+            <InpageLink to="#data">Data</InpageLink>
+            <InpageLink to="#resources">Resources</InpageLink>
+          </ul>
+        </nav>
+        <div style={{ display: `flex` }}>
+          <div style={{ padding: `0 1rem`, borderLeft: `1px solid #9E9E9E` }}>
+            <a href={campaign.website}>Primary Website</a>
+          </div>
+          <div style={{ padding: `0 1rem`, borderLeft: `1px solid #9E9E9E` }}>
+            <a href={campaign.website}>Data</a>
+          </div>
+        </div>
+      </div>
+      <section id="overview">
+        <h2>Overview</h2>
+      </section>
+      <section id="milestones">
+        <h2>Milestones</h2>
+      </section>
+      <section id="platforms">
+        <h2>Platforms & Instruments</h2>
+      </section>
+      <section id="data">
+        <h2>Data</h2>
+      </section>
+      <section id="resources">
+        <h2>Additional Information</h2>
+      </section>
     </Layout>
   )
 }
@@ -58,6 +106,7 @@ export const query = graphql`
       focus: NASA_Earth_Science_Focus_Areas
       countDeployments: Number_of_Deployments
       countDataproducts: Number_of_Published_Data_Products
+      website: Repository_Primary_Website__DAAC_homepage_
     }
   }
 `
