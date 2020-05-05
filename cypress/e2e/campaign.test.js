c273061a88543b2c17284ffaae8e9f40bdeb2270
Alice Rühl <alice@ds.io>
2020-05-05 21:25:26 +0200

    Add overview section to campaign template

--- cypress/e2e/campaign.test.js
+++ cypress/e2e/campaign.test.js
@@ -1,50 +1,109 @@
 /// <reference types="Cypress" />
 
 describe("Campaign", () => {
-  beforeEach(() => {
+  before(() => {
     cy.visit("/campaign/6d179d67-ebd8-5750-9f5d-dc55c92697b9")
   })
-  it("renders correctly", () => {
-    cy.get("main").find("header").find("p").contains("ABoVE").should("exist")
 
-    cy.get("main")
-      .find("header")
-      .find("h1")
-      .contains("Arctic-Boreal Vulnerability Experiment")
-      .should("exist")
+  describe("the header", () => {
+    it("displays the short name", () => {
+      cy.get("main").find("header").find("p").contains("ABoVE").should("exist")
+    })
 
-    cy.get("main")
-      .find("header")
-      .find("p")
-      .contains("Carbon Cycle & Ecosystems, Climate Variability & Change")
-      .should("exist")
+    it("displays the long name as title", () => {
+      cy.get("main")
+        .find("header")
+        .find("h1")
+        .contains("Arctic-Boreal Vulnerability Experiment")
+        .should("exist")
+    })
 
-    cy.get("main")
-      .find("header")
-      .find("[data-cy=stats]")
-      .find("dt")
-      .should($stat => {
-        expect($stat, "3 items").to.have.length(3)
-        expect($stat.eq(0), "first item").to.contain("Deployments")
-        expect($stat.eq(1), "second item").to.contain("Flights")
-        expect($stat.eq(2), "third item").to.contain("Data Products")
-      })
+    it("displays the focus area as subtitle", () => {
+      cy.get("main")
+        .find("header")
+        .find("p")
+        .contains("Carbon Cycle & Ecosystems, Climate Variability & Change")
+        .should("exist")
+    })
+    it("displays 3 big numbers", () => {
+      cy.get("main")
+        .find("header")
+        .find("[data-cy=stats]")
+        .find("dd")
+        .should($stat => {
+          expect($stat, "3 items").to.have.length(3)
+          expect($stat.eq(0), "first item").to.contain("Deployments")
+          expect($stat.eq(1), "second item").to.contain("Flights")
+          expect($stat.eq(2), "third item").to.contain("Data Products")
+        })
+    })
+    it("displays a map", () => {
+      cy.get("main")
+        .find("header")
+        .find("[data-cy=overview-map]")
+        .should("exist")
+    })
+  })
 
-    cy.get("main").find("header").find("[data-cy=overview-map]").should("exist")
+  describe("the inpage nav", () => {
+    it("exists", () => {
+      cy.get("main").find("nav").should("exist")
+    })
 
-    cy.get("main").find("nav").should("exist")
-    cy.get("main")
-      .find("nav")
-      .find("a")
-      .should($anchor => {
-        expect($anchor, "5 items").to.have.length(5)
-        expect($anchor.eq(0), "first item").to.contain("Overview")
-        expect($anchor.eq(1), "first item").to.contain("Milestones")
-        expect($anchor.eq(2), "first item").to.contain(
-          "Platforms & Instruments"
-        )
-        expect($anchor.eq(3), "first item").to.contain("Data")
-        expect($anchor.eq(4), "first item").to.contain("Resources")
-      })
+    it("has 5 items", () => {
+      cy.get("main")
+        .find("nav")
+        .find("a")
+        .should($anchor => {
+          expect($anchor, "5 items").to.have.length(5)
+          expect($anchor.eq(0), "first item").to.contain("Overview")
+          expect($anchor.eq(1), "first item").to.contain("Milestones")
+          expect($anchor.eq(2), "first item").to.contain(
+            "Platforms & Instruments"
+          )
+          expect($anchor.eq(3), "first item").to.contain("Data")
+          expect($anchor.eq(4), "first item").to.contain("Resources")
+        })
+    })
+  })
+
+  describe("the overview section", () => {
+    it("exists", () => {
+      cy.get("[data-cy=overview-section]").should("exist")
+    })
+
+    it("has a heading", () => {
+      cy.get("[data-cy=overview-section]")
+        .find("h2")
+        .should("have.text", "Overview")
+    })
+
+    it("displays a description", () => {
+      cy.get("[data-cy=description]").should("exist")
+    })
+
+    it("displays some facts", () => {
+      cy.get("[data-cy=overview-fact]")
+        .find("label")
+        .should($label => {
+          expect($label, "4 items").to.have.length(4)
+
+          expect($label.eq(0), "first item").to.contain("Study dates")
+          expect($label.eq(1), "first item").to.contain("Region")
+          expect($label.eq(2), "first item").to.contain("Season of Study")
+          expect($label.eq(3), "first item").to.contain("Spatial bounds")
+        })
+    })
+
+    it("displays word lists", () => {
+      cy.get("[data-cy=word-list]")
+        .find("label")
+        .should($label => {
+          expect($label, "2 items").to.have.length(2)
+
+          expect($label.eq(0), "first item").to.contain("Focus Phenomena")
+          expect($label.eq(1), "first item").to.contain("Science Keywords")
+        })
+    })
   })
 })
--- src/templates/campaign.js
+++ src/templates/campaign.js
@@ -4,94 +4,187 @@ import Layout from "../components/layout"
 
 import NorthAmerica from "../data/north-america.svg"
 
-const StatNumber = ({ number = "--", label }) => (
-  <>
-    <dd style={{ fontSize: `3rem` }}>{number.length === 0 ? "--" : number}</dd>
-    <dt style={{ gridRowStart: 2, textTransform: `uppercase` }}>{label}</dt>
-  </>
-)
+const Header = ({
+  shortname,
+  longname,
+  focus,
+  countDeployments,
+  countDataproducts,
+}) => {
+  const StatNumber = ({ number = "--", label }) => (
+    <>
+      <dt style={{ fontSize: `3rem` }}>
+        {number.length === 0 ? "--" : number}
+      </dt>
+      <dd style={{ gridRowStart: 2, textTransform: `uppercase` }}>{label}</dd>
+    </>
+  )
 
-const InpageLink = props => (
-  <li style={{ margin: `0 1rem 0 0` }}>
-    <a href={props.to}>{props.children}</a>
-  </li>
-)
+  return (
+    <header style={{ display: `flex` }}>
+      <div style={{ flex: `1.61803398875` }}>
+        <div>
+          <p>{shortname}</p>
+          <h1>{longname}</h1>
+          <p>{focus}</p>
+        </div>
+        <dl style={{ display: `grid` }} data-cy="stats">
+          <StatNumber number={countDeployments} label="Deployments" />
+          <StatNumber number="--" label="Flights" />
+          <StatNumber number={countDataproducts} label="Data Products" />
+        </dl>
+      </div>
+      <div style={{ flex: `1` }}>
+        <img src={NorthAmerica} alt="North America" data-cy="overview-map" />
+      </div>
+    </header>
+  )
+}
+
+const InpageNav = ({ website }) => {
+  const InpageLink = props => (
+    <li style={{ margin: `0 1rem 0 0` }}>
+      <a href={props.to}>{props.children}</a>
+    </li>
+  )
+
+  return (
+    <div
+      style={{
+        display: `flex`,
+        padding: `1rem`,
+        borderTop: `1px solid #9E9E9E`,
+        borderBottom: `1px solid #9E9E9E`,
+        justifyContent: `space-between`,
+        alignItems: `center`,
+      }}
+    >
+      <nav role="inpage-scroll">
+        <ul
+          style={{
+            display: `flex`,
+            flexDirection: `row`,
+            justifyContent: `flex-start`,
+            margin: 0,
+            listStyle: `none`,
+          }}
+        >
+          <InpageLink to="#overview">Overview</InpageLink>
+          <InpageLink to="#milestones">Milestones</InpageLink>
+          <InpageLink to="#platforms">Platforms & Instruments</InpageLink>
+          <InpageLink to="#data">Data</InpageLink>
+          <InpageLink to="#resources">Resources</InpageLink>
+        </ul>
+      </nav>
+      <div style={{ display: `flex` }}>
+        <div style={{ padding: `0 1rem`, borderLeft: `1px solid #9E9E9E` }}>
+          <a href={website}>Primary Website</a>
+        </div>
+        <div style={{ padding: `0 1rem`, borderLeft: `1px solid #9E9E9E` }}>
+          <a href={website}>Data</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const OverviewSection = ({
+  description,
+  startdate,
+  enddate,
+  region,
+  season,
+  bounds,
+  focusPenomena,
+  keywords,
+}) => {
+  const FactItem = ({ label, fact }) => (
+    <div data-cy="overview-fact">
+      <label
+        style={{
+          textTransform: `uppercase`,
+          color: `#9E9E9E`,
+        }}
+      >
+        {label}
+      </label>
+      <p style={{ margin: 0 }}>{fact}</p>
+    </div>
+  )
+
+  const WordList = ({ label, list }) => (
+    <>
+      <label style={{ textTransform: `uppercase`, color: `#9E9E9E` }}>
+        {label}
+      </label>
+      <p>{list}</p>
+    </>
+  )
+
+  return (
+    <section id="overview" data-cy="overview-section">
+      <h2>Overview</h2>
+      <div style={{ display: `flex` }}>
+        <div style={{ flex: `1.61803398875` }}>
+          <p data-cy="description">{description}</p>
+          <div
+            style={{
+              display: `grid`,
+              gap: `0.5rem`,
+              gridAutoFlow: `column`,
+              gridTemplateColumns: ` 1fr 1fr`,
+              gridTemplateRows: ` 1fr 1fr`,
+            }}
+          >
+            <FactItem label="Study dates" fact={`${startdate} - ${enddate}`} />
+            <FactItem label="Region" fact={region} />
+            <FactItem label="Season of Study" fact={season} />
+            <FactItem label="Spatial bounds" fact={bounds} />
+          </div>
+        </div>
+        <div
+          style={{ flex: `1`, padding: `1rem`, backgroundColor: `#FBFBFB` }}
+          data-cy="word-list"
+        >
+          <WordList label="Focus Phenomena" list={focusPenomena} />
+          <WordList label="Science Keywords" list={keywords} />
+        </div>
+      </div>
+    </section>
+  )
+}
 
 const CampaignTemplate = ({ data: { campaign } }) => {
   return (
     <Layout>
-      <header style={{ display: `flex` }}>
-        <div style={{ flex: `1.61803398875` }}>
-          <div>
-            <p>{campaign.shortname}</p>
-            <h1>{campaign.longname}</h1>
-            <p>{campaign.focus}</p>
-          </div>
-          <dl style={{ display: `grid` }} data-cy="stats">
-            <StatNumber
-              number={campaign.countDeployments}
-              label="Deployments"
-            />
-            <StatNumber number="--" label="Flights" />
-            <StatNumber
-              number={campaign.countDataproducts}
-              label="Data Products"
-            />
-          </dl>
-        </div>
-        <div style={{ flex: `1` }}>
-          <img src={NorthAmerica} alt="North America" data-cy="overview-map" />
-        </div>
-      </header>
-      <div
-        style={{
-          display: `flex`,
-          padding: `1rem`,
-          borderTop: `1px solid #9E9E9E`,
-          borderBottom: `1px solid #9E9E9E`,
-          justifyContent: `space-between`,
-          alignItems: `center`,
-        }}
-      >
-        <nav role="inpage-scroll">
-          <ul
-            style={{
-              display: `flex`,
-              flexDirection: `row`,
-              justifyContent: `flex-start`,
-              margin: 0,
-              listStyle: `none`,
-            }}
-          >
-            <InpageLink to="#overview">Overview</InpageLink>
-            <InpageLink to="#milestones">Milestones</InpageLink>
-            <InpageLink to="#platforms">Platforms & Instruments</InpageLink>
-            <InpageLink to="#data">Data</InpageLink>
-            <InpageLink to="#resources">Resources</InpageLink>
-          </ul>
-        </nav>
-        <div style={{ display: `flex` }}>
-          <div style={{ padding: `0 1rem`, borderLeft: `1px solid #9E9E9E` }}>
-            <a href={campaign.website}>Primary Website</a>
-          </div>
-          <div style={{ padding: `0 1rem`, borderLeft: `1px solid #9E9E9E` }}>
-            <a href={campaign.website}>Data</a>
-          </div>
-        </div>
-      </div>
-      <section id="overview">
-        <h2>Overview</h2>
-      </section>
-      <section id="milestones">
+      <Header
+        shortname={campaign.shortname}
+        longname={campaign.longname}
+        focus={campaign.focus}
+        countDeployments={campaign.countDeployments}
+        countDataproducts={campaign.countDataproducts}
+      />
+      <InpageNav website={campaign.website} />
+      <OverviewSection
+        description={campaign.description}
+        startdate={campaign.startdate}
+        enddate={campaign.enddate}
+        region={campaign.region}
+        season={campaign.season}
+        bounds={campaign.bounds}
+        focusPenomena={campaign.focusPenomena}
+        keywords={campaign.keywords}
+      />
+      <section id="milestones" data-cy="milestones-section">
         <h2>Milestones</h2>
       </section>
-      <section id="platforms">
+      <section id="platforms" data-cy="platforms-section">
         <h2>Platforms & Instruments</h2>
       </section>
-      <section id="data">
+      <section id="data" data-cy="data-section">
         <h2>Data</h2>
       </section>
-      <section id="resources">
+      <section id="resources" data-cy="resources-section">
         <h2>Additional Information</h2>
       </section>
     </Layout>
@@ -107,6 +200,14 @@ export const query = graphql`
       countDeployments: Number_of_Deployments
       countDataproducts: Number_of_Published_Data_Products
       website: Repository_Primary_Website__DAAC_homepage_
+      description: Description___DRAFT
+      focusPenomena: Scientific_Objective_Focus_Phenomena
+      keywords: Campaign_Geophysical_Phenomena_Studied__from_GCMD_variable_list_
+      startdate: Campaign_Start_Date__date_of_first_deployment_start_
+      enddate: Campaign_End_Date__date_of_last_deployment_end_
+      region: Region_of_Campaign_Description
+      season: Season_s__of_Study
+      bounds: Campaign_Spatial_Bounds__N_S_E_W_lat_lon_
     }
   }
 `
