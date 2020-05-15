24ee99da79ea022a57175e176ddd80e806d098a5
Alice Rühl <alice@ds.io>
2020-05-15 17:57:30 +0200

    Temporary fix empty explore pages

--- src/pages/explore/platforms.js
+++ src/pages/explore/platforms.js
@@ -1,12 +1,10 @@
 import React from "react"
 
 import Layout from "../../components/layout"
-import ExploreMenu from "../../components/explore-menu"
 import ExploreSection from "../../components/explore-section"
 
 const Platforms = () => (
   <Layout>
-    <ExploreMenu />
     <ExploreSection category="platforms" />
   </Layout>
 )
