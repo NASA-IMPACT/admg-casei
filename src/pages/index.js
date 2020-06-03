8bb5f359762fb1f6a1a51db083210929e185ca16
Alice Rühl <alice@ds.io>
2020-06-03 20:35:42 +0200

    Start work on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,45 @@
 import React from "react"
-import { Link } from "gatsby"
-import { Button } from "@devseed-ui/button"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Image from "../components/image"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Welcome</h1>
-    <p>A centralized airborne data inventory.</p>
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
 
-    <Button variation="base-raised-dark" size="large">
-      <Link to="/explore/campaigns">Explore airborne campaigns</Link>
-    </Button>
-  </Layout>
-)
+  return (
+    <Layout>
+      <SEO title="Home" />
+
+      <div style={{ display: `grid`, gridTemplateColumns: `1fr 1fr` }}>
+        <div style={{ alignSelf: `end` }}>
+          <div style={{ marginBottom: `-30px` }}>NASA</div>
+          <h1>{data.site.siteMetadata.title}</h1>
+        </div>
+        <div style={{ alignSelf: `start` }}>
+          <p>
+            Explore NASA’s catalog of airborne, <br /> field stationary and
+            fixed campaigns.
+          </p>
+        </div>
+        <div style={{ gridArea: `1 / 2 / 3 / 3` }}>
+          <Image
+            filename="globe.png"
+            alt={`a globe displaying natural features and slight cloud coverage`}
+          />
+        </div>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
