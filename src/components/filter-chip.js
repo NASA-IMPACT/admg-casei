e037d83119dda155cca2800db5733167368da034
Necoline <necoline@developmentseed.org>
2020-06-16 21:24:35 -0400

    replaces x icon with collecticon btn

--- src/components/filter-chip.js
+++ src/components/filter-chip.js
@@ -2,25 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 
 import theme from "../utils/theme"
-
-// TODO: figure out how to import and use collecticons directly
-const CloseIcon = ({ color = "#FFF" }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="12px"
-    height="12px"
-    viewBox="0 0 16 16"
-  >
-    <polygon
-      fill={color}
-      points="13.707,3.707 12.293,2.293 8,6.586 3.707,2.293 2.293,3.707 6.586,8 2.293,12.293 3.707,13.707 8,9.414  12.293,13.707 13.707,12.293 9.414,8"
-    />
-  </svg>
-)
-
-CloseIcon.propTypes = {
-  color: PropTypes.string,
-}
+import { Button } from "@devseed-ui/button"
 
 const FilterChip = ({ id, label, removeFilter }) => (
   <div
@@ -36,24 +18,19 @@ const FilterChip = ({ id, label, removeFilter }) => (
     data-cy="filter-chip"
   >
     <small>{label}</small>
-    <button
-      type="button"
-      onClick={() => removeFilter(id)}
-      style={{
-        background: "none",
-        border: "none",
-        flexGrow: 0,
-        cursor: `pointer`,
-        color: theme.type.base.color,
-        verticalAlign: `middle`,
-        marginLeft: `0.5rem`,
-      }}
+    <Button
       data-cy="remove-filter"
+      useIcon="xmark"
+      variation="achromic-plain"
+      hideText
+      size="small"
+      id="close-icon"
+      title="remove filter"
+      style={{ marginLeft: `0.5rem` }}
+      onClick={() => removeFilter(id)}
     >
-      <span role="img" aria-label="close-icon">
-        <CloseIcon color={theme.type.base.color} />
-      </span>
-    </button>
+      close icon
+    </Button>
   </div>
 )
 
