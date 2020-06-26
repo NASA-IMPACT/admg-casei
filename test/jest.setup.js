20ec5af02bbc5a24c561bf6121226c5ab9e1a08c
Necoline <necoline@developmentseed.org>
2020-06-26 08:19:04 -0400

    remove template logic

--- test/jest.setup.js
+++ test/jest.setup.js
@@ -1,14 +1,1 @@
 import "jest-styled-components"
-
-global.MutationObserver = class {
-  constructor() {}
-  disconnect() {}
-  observe() {}
-}
-
-global.IntersectionObserver = class {
-  constructor() {}
-  observe() {}
-  unobserve() {}
-  disconnect() {}
-}
