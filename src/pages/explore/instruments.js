b26b4428e1e1780c75b4490f9feec644b76d35e2
Necoline <necoline@developmentseed.org>
2020-06-30 17:37:33 -0400

    add initial query

--- src/pages/explore/instruments.js
+++ src/pages/explore/instruments.js
@@ -1,14 +1,24 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../../components/layout"
 import ExploreMenu from "../../components/explore-menu"
 import ExploreSection from "../../components/explore-section"
 
-const Instruments = () => (
-  <Layout>
-    <ExploreMenu />
-    <ExploreSection category="instruments" />
-  </Layout>
-)
+export default function Instruments({ data }) {
+  console.log("data", data)
+  return (
+    <Layout>
+      <ExploreMenu />
+      <ExploreSection category="instruments" />
+    </Layout>
+  )
+}
 
-export default Instruments
+export const query = graphql`
+  query {
+    all: allInstrument(sort: { order: ASC, fields: short_name }) {
+      totalCount
+    }
+  }
+`
