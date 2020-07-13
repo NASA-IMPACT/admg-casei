87a0f7c4287511928461159b3a072ef7103da2e2
Necoline <necoline@developmentseed.org>
2020-07-13 17:41:26 -0400

    add tests

---
+++ cypress/e2e/glossary.test.js
@@ -0,0 +1,28 @@
+/// <reference types="Cypress" />
+
+describe("About", () => {
+  beforeEach(() => {
+    cy.visit("/glossary")
+  })
+  it("it renders correctly", () => {
+    cy.get("main").find("h1").contains("Glossary")
+    cy.get("main").find("section").should("have.length", 1)
+  })
+  describe("the glossary", () => {
+    it("should display terms and definitions", () => {
+      cy.get("[data-cy=glossary-definition-list]").each($el => {
+        cy.get($el).find("dl").should("exist")
+        cy.get($el)
+          .find("dt")
+          .should($dt => {
+            expect($dt, "16 terms").to.have.length(16)
+          })
+        cy.get($el)
+          .find("dd")
+          .should($dd => {
+            expect($dd, "16 terms").to.have.length(16)
+          })
+      })
+    })
+  })
+})
