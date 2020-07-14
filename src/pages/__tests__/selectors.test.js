391892cd27a3f444d5891642e1323862ef802816
Alice Rühl <alice@ds.io>
2020-07-14 10:47:20 +0200

    Export fixtures together from index file

--- src/pages/__tests__/selectors.test.js
+++ src/pages/__tests__/selectors.test.js
@@ -7,10 +7,7 @@ import theme from "../../utils/theme"
 
 import Campaigns from "../explore/campaigns"
 import { selector } from "../../utils/filter-utils"
-import { query } from "../../../test/__fixtures__/campaign-query"
-import { location } from "../../../test/__fixtures__/location"
-import { site } from "../../../test/__fixtures__/site"
-import { images } from "../../../test/__fixtures__/images"
+import { query, location, site, images } from "../../../test/__fixtures__"
 
 const useStaticQuery = jest.spyOn(Gatsby, "useStaticQuery")
 useStaticQuery.mockImplementation(() => ({
