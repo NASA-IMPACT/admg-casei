0c63e41fe214383a636f2c637003874b2a44cf65
Alice Rühl <alice@ds.io>
2020-07-27 13:52:08 +0200

    Point cypress to text/__fixtures__
    and convert fixtures to json files

--- test/__fixtures__/site.js
+++
@@ -1,8 +0,0 @@
-export const site = {
-  siteMetadata: {
-    title: `Catalog of Archived Sub-orbital Earth Science Investigations`,
-    shortname: `NASA | CASEI`,
-    description: `Explore NASA’s catalog of airborne, field stationary and fixed campaigns.`,
-    author: `@developmentseed`,
-  },
-}
