7d9a47361257f7fcef2f4844829298a38315366b
Alice Rühl <alice@ds.io>
2020-07-28 22:44:30 +0200

    Wrap root elements in ssr as well

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,8 @@
  * Implement Gatsby's Browser APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
+ *
+ * Note: There is an equivalent hook in Gatsby’s SSR API. It is recommended to use both APIs together.
  */
 
 import React from "react"
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,6 +2,27 @@
  * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
+ *
+ * Note: There is an equivalent hook in Gatsby’s Browser API. It is recommended to use both APIs together.
  */
 
-// You can delete this file if you're not using it
+import React from "react"
+import PropTypes from "prop-types"
+import { DevseedUiThemeProvider } from "@devseed-ui/helpers"
+
+import theme from "./src/utils/theme"
+import GlobalStyles from "./src/components/global-styles"
+import AuthProvider from "./src/components/auth-provider"
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <DevseedUiThemeProvider theme={theme}>
+      <GlobalStyles />
+      <AuthProvider>{element}</AuthProvider>
+    </DevseedUiThemeProvider>
+  )
+}
+
+wrapRootElement.propTypes = {
+  element: PropTypes.element,
+}
