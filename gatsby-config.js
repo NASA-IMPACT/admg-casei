fc33e4d156b4b41a2a97dd39556d8ca30b2756fc
Necoline <necoline@developmentseed.org>
2020-08-11 11:44:04 -0400

    configure json filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content/`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
