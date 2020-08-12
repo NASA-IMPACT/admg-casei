bad1f71f838318526bb41963d9354dad2e192d8b
Necoline <necoline@developmentseed.org>
2020-08-12 16:54:37 -0400

    adds mock img to test

--- src/components/__tests__/milestone.js
+++ src/components/__tests__/milestone.js
@@ -2,6 +2,11 @@ import React from "react"
 import renderer from "react-test-renderer"
 import Milestone from "../timeline/milestone"
 
+jest.mock("../image.js", () => {
+  const mockImage = () => <img />
+  return mockImage
+})
+
 describe("MileStone", () => {
   it("renders correctly", () => {
     const tree = renderer
@@ -9,11 +14,12 @@ describe("MileStone", () => {
         <Milestone
           key="f2c42b8a-2c06-407c-9345-aa9824ac65f5"
           type="deployment"
-          startDate="2012-10-02"
-          endDate="2012-10-20"
+          daterange="10/02/2012 - 10/20/2012"
           name="Canada, Oregon Fall 2012"
           details="2 Flights"
           region="Canada"
+          placeholderImageUrl="AFRC2016-0292-08~orig.jpg"
+          placeholderImageAlt="text"
         />
       )
       .toJSON()
