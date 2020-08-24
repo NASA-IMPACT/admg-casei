f2525297723985c2b62b0bef1fb4dcdebae5f1b1
Necoline <necoline@developmentseed.org>
2020-08-24 16:59:22 -0400

    moves css import to global-styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,6 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import "@reach/listbox/styles.css"
 
 import Header from "./header"
 import Nav from "./nav"
