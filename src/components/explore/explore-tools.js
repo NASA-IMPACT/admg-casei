04036639a5f118d499e293084a8a7820b31c8dd5
Necoline <necoline@developmentseed.org>
2020-09-11 14:49:59 -0400

    adds clear btn

--- src/components/explore/explore-tools.js
+++ src/components/explore/explore-tools.js
@@ -6,6 +6,26 @@ import Searchbar from "./searchbar"
 import Chip from "../chip"
 import SortMenu from "./sort-menu"
 
+const ClearIcon = ({ color = "#FFF" }) => (
+  <svg
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    width="16px"
+    height="16px"
+    viewBox="0 0 16 16"
+  >
+    <rect width="16px" height="16px" id="icon-bound" fill="none" />
+    <path
+      fill={color}
+      d="M11,5h2v8.5c0,0.825-0.675,1.5-1.5,1.5h-7C3.675,15,3,14.325,3,13.5V5h2v8h2V5h2v8h2V5z M2,2h12v2H2V2z M6,0h4v1H6V0z"
+    />
+  </svg>
+)
+
+ClearIcon.propTypes = {
+  color: PropTypes.string,
+}
+
 const ExploreTools = React.forwardRef(
   (
     {
@@ -15,6 +35,7 @@ const ExploreTools = React.forwardRef(
       getFilterLabelById,
       getFilterOptionsById,
       removeFilter,
+      clearFilters,
       sortOrder,
       setSortOrder,
       category,
@@ -57,6 +78,24 @@ const ExploreTools = React.forwardRef(
                 chipAction={removeFilter}
               />
             ))}
+            <button
+              type="button"
+              onClick={() => clearFilters()}
+              style={{
+                background: "none",
+                border: "none",
+                flexGrow: 0,
+                cursor: `pointer`,
+                // color: theme.color.base,
+                verticalAlign: `middle`,
+                marginLeft: `0.5rem`,
+              }}
+              data-cy="remove-filter"
+            >
+              <span role="img" aria-label="clear-icon">
+                <ClearIcon />
+              </span>
+            </button>
           </div>
         )}
       </>
@@ -71,6 +110,7 @@ ExploreTools.propTypes = {
   getFilterLabelById: PropTypes.func.isRequired,
   getFilterOptionsById: PropTypes.func.isRequired,
   removeFilter: PropTypes.func.isRequired,
+  clearFilters: PropTypes.func.isRequired,
   sortOrder: PropTypes.string.isRequired,
   setSortOrder: PropTypes.func.isRequired,
   category: PropTypes.oneOf(["campaigns", "platforms", "instruments"])
