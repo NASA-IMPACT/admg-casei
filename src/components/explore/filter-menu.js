947f5fac44f0d2aa1a36d3f66e39923db28ee750
Necoline <necoline@developmentseed.org>
2020-09-15 13:58:13 -0400

    adds highlight for selected filter option

--- src/components/explore/filter-menu.js
+++ src/components/explore/filter-menu.js
@@ -11,11 +11,20 @@ import VisuallyHidden from "@reach/visually-hidden"
 
 import theme from "../../utils/theme"
 
-const Filter = ({ label, options }) => (
+const Filter = ({ label, options, selectedFilterIds }) => (
   <>
     <strong>{label}</strong>
     {options.map(o => (
-      <ListboxOption key={o.id} value={o.id} data-cy="filter-option">
+      <ListboxOption
+        key={o.id}
+        value={o.id}
+        data-cy="filter-option"
+        style={{
+          backgroundColor: selectedFilterIds.includes(o.id)
+            ? theme.color.secondary
+            : theme.color.primary,
+        }}
+      >
         {o.shortname}
       </ListboxOption>
     ))}
@@ -30,6 +39,7 @@ Filter.propTypes = {
       shortname: PropTypes.string,
     })
   ).isRequired,
+  selectedFilterIds: PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
 const FilterMenu = ({
@@ -88,31 +98,37 @@ const FilterMenu = ({
                   id="focus"
                   label="Focus Area"
                   options={getFilterOptionsById("focus")}
+                  selectedFilterIds={selectedFilterIds}
                 />
                 <Filter
                   id="geophysical"
                   label="Geophysical Concept"
                   options={getFilterOptionsById("geophysical")}
+                  selectedFilterIds={selectedFilterIds}
                 />
                 <Filter
                   id="season"
                   label="Season"
                   options={getFilterOptionsById("season")}
+                  selectedFilterIds={selectedFilterIds}
                 />
                 <Filter
                   id="region"
                   label="Geographical Region"
                   options={getFilterOptionsById("region")}
+                  selectedFilterIds={selectedFilterIds}
                 />
                 <Filter
                   id="platform"
                   label="Platform"
                   options={getFilterOptionsById("platform")}
+                  selectedFilterIds={selectedFilterIds}
                 />
                 <Filter
                   id="funding"
                   label="Funding Agency"
                   options={getFilterOptionsById("funding")}
+                  selectedFilterIds={selectedFilterIds}
                 />
               </>
             )}
