01e174b5d3e84da481474fa970dd041d58e3fc70
Necoline <necoline@developmentseed.org>
2020-09-26 13:38:02 -0400

    adds style and loader

--- src/templates/campaign/hero.js
+++ src/templates/campaign/hero.js
@@ -1,14 +1,13 @@
-import React, { useRef } from "react"
+import React, { useRef, useLayoutEffect, useState } from "react"
 import PropTypes from "prop-types"
 import { graphql } from "gatsby"
 import parse from "wellknown"
 import * as turf from "@turf/turf"
-import geoViewport from "@mapbox/geo-viewport"
+import mapbox from "mapbox-gl"
+// import HeroStats from "../../components/hero-stats"
 
-import HeroStats from "../../components/hero-stats"
-
-import { useContainerDimensions } from "../../utils/helpers"
-import theme from "../../utils/theme"
+// import { useContainerDimensions } from "../../utils/helpers"
+// import theme from "../../utils/theme"
 
 const CampaignHero = ({
   bounds,
@@ -19,84 +18,65 @@ const CampaignHero = ({
   countCollectionPeriods,
   countDataProducts,
 }) => {
-  const geometry = parse(bounds)
-  const geojson = {
-    type: "Feature",
-    properties: {
-      stroke: "#f25d0d",
-      "stroke-width": 2,
-      "fill-opacity": 0,
-    },
-    geometry: geometry,
-  }
-  const [w, s, e, n] = turf.bbox(geometry)
-  const west = turf.point([w, (n + s) / 2])
-  const east = turf.point([e, (n + s) / 2])
-  const options = { units: "degrees" }
-  const distance = turf.distance(west, east, options)
-  const offsetCoords = turf.transformTranslate(
-    geometry,
-    distance * 0.5,
-    -90,
-    options
-  )
-
-  const scaledCoords = turf.bbox(
-    turf.transformScale(offsetCoords, 1.4, options)
-  )
-
+  const [map, setMap] = useState(null)
   const containerRef = useRef()
-  const { width } = useContainerDimensions(containerRef)
 
-  const size = [Math.min(width, 1280), 560]
-  const viewport = geoViewport.viewport(scaledCoords, size)
-  const overlay = encodeURIComponent(JSON.stringify(geojson))
-  const accessToken =
-    "pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJja2JxbjJhbGQybnpnMnJwdnk0NXloMmt1In0.5ciMNUW3yaadjwmlDLTugw"
+  const geometry = parse(bounds)
+  const coordinates = geometry.coordinates.shift()
+  const features = turf.featureCollection(
+    coordinates.map(coor => turf.point(coor))
+  )
 
-  const url = width
-    ? `https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/static/geojson(${overlay})/${viewport.center.join(
-        ","
-      )},${viewport.zoom}/${size.join("x")}?access_token=${accessToken}`
-    : null // skip while width is to be determinded
+  useLayoutEffect(() => {
+    const m = new mapbox.Map({
+      container: containerRef.current,
+      style: `mapbox://styles/mapbox/satellite-streets-v11/`,
+      zoom: 13,
+      center: turf.center(features).geometry.coordinates,
+    })
+    m.on("load", () => {
+      m.addSource("campaign-bbox", {
+        type: "geojson",
+        data: {
+          type: "Feature",
+          properties: {
+            stroke: "#f25d0d",
+            "stroke-width": 2,
+            "fill-opacity": 0,
+          },
+          geometry: {
+            type: "Polygon",
+            coordinates: coordinates,
+          },
+        },
+      })
+      m.addLayer({
+        id: "campaign-bbox",
+        type: "line",
+        source: "campaign-bbox",
+        layout: {},
+        paint: {
+          "line-color": "#f25d0d",
+          "line-opacity": 0.8,
+          "line-width": 2,
+        },
+      })
+    })
+    setMap(m)
+
+    return () => {
+      if (map) {
+        map.remove()
+      }
+    }
+  }, [])
 
   return (
     <section
       data-cy="campaign-hero"
       ref={containerRef}
-      style={{
-        backgroundImage: `linear-gradient(90deg, rgba(12,21,32, 0.8) 0%, rgba(12,21,32, 0.7)50%, rgba(12,21,32, 0.0)66%), url("${url}")`,
-        backgroundPosition: `center`,
-        backgroundSize: `cover`,
-        backgroundRepeat: `no-repeat`,
-        height: `35rem`,
-      }}
-    >
-      <div
-        style={{
-          display: `flex`,
-          maxWidth: theme.layout.maxWidth,
-          margin: `0 auto`,
-          paddingTop: `11rem`,
-        }}
-      >
-        <div style={{ flex: `2`, padding: `0 ${theme.layout.pageMargin}` }}>
-          <div>
-            <p>{shortname}</p>
-            <h1 data-cy="campaign-hero-header">{longname}</h1>
-            <p>{focusListing}</p>
-          </div>
-          <HeroStats
-            statList={[
-              { number: countDeployments, label: "Deployments" },
-              { number: countCollectionPeriods, label: "Collection Periods" },
-              { number: countDataProducts, label: "Data Products" },
-            ]}
-          />
-        </div>
-        <div style={{ flex: `1` }}></div>
-      </div>
-    </section>
+      style={{ height: `35rem` }}
+    ></section>
   )
 }
 
