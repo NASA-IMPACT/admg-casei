4cf6e709c5a3fc772baa64a72d4cd2d2cf622a44
Necoline <necoline@developmentseed.org>
2020-09-29 14:51:35 -0400

    adds text overflow and hover text

--- src/components/section/content-item.js
+++ src/components/section/content-item.js
@@ -4,17 +4,36 @@ import PropTypes from "prop-types"
 import Label from "../label"
 import ExternalLink from "../external-link"
 
-export default function ContentItem({ id, label, info = "N/A", link }) {
+export default function ContentItem({
+  id,
+  label,
+  info = "N/A",
+  link,
+  shortenText,
+}) {
+  const text =
+    info && shortenText ? (
+      <p
+        data-cy={`${id}-text`}
+        style={{
+          whiteSpace: `nowrap`,
+          overflow: `hidden`,
+          textOverflow: `ellipsis`,
+          maxWidth: `250px`,
+        }}
+        title={info}
+      >
+        {info}
+      </p>
+    ) : (
+      <p data-cy={`${id}-text`}>{info || "N/A"}</p>
+    )
   return (
     <div data-cy={id}>
       <Label id={id} showBorder>
         {label}
       </Label>
-      {link ? (
-        <ExternalLink id={id} label={info} url={link} />
-      ) : (
-        <p data-cy={`${id}-text`}>{info || "N/A"}</p>
-      )}
+      {link ? <ExternalLink id={id} label={info} url={link} /> : text}
     </div>
   )
 }
@@ -24,4 +43,9 @@ ContentItem.propTypes = {
   label: PropTypes.string.isRequired,
   info: PropTypes.string,
   link: PropTypes.string,
+  shortenText: PropTypes.bool,
+}
+
+ContentItem.defaultProps = {
+  shortenText: false,
 }
