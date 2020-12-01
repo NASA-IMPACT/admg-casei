6122ea97670a5dddffdeb4fcb9c3da62a719af10
Alice Rühl <alice@developmentseed.org>
2020-12-01 12:12:39 +0100

    Fix filter if no longname is provided

--- src/components/filter/filter-box.js
+++ src/components/filter/filter-box.js
@@ -54,21 +54,20 @@ export default function Filter({
         </ListboxButton>
         <ListboxPopover style={{ background: theme.color.primary }}>
           <ListboxList data-cy="data-products-filter-options">
-            {filterOptions.map(o => (
-              <ListboxOption
-                key={o.id}
-                value={o.longname}
-                data-cy="filter-option"
-              >
-                {o.longname.toUpperCase()}
-                {selectedFilterIds.includes(o.longname) && (
-                  <IconButton
-                    id="remove-filter"
-                    icon={<CloseIcon color={theme.color.base} />}
-                  />
-                )}
-              </ListboxOption>
-            ))}
+            {filterOptions.map(o => {
+              const value = o.longname || o.shortname
+              return (
+                <ListboxOption key={o.id} value={value} data-cy="filter-option">
+                  {value.toUpperCase()}
+                  {selectedFilterIds.includes(value) && (
+                    <IconButton
+                      id="remove-filter"
+                      icon={<CloseIcon color={theme.color.base} />}
+                    />
+                  )}
+                </ListboxOption>
+              )
+            })}
           </ListboxList>
         </ListboxPopover>
       </ListboxInput>
@@ -77,7 +76,18 @@ export default function Filter({
 }
 
 Filter.propTypes = {
-  filterOptions: PropTypes.array.isRequired,
+  filterOptions: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      longname: PropTypes.string.isRequired,
+      shortname: PropTypes.string.isRequired,
+      dois: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.string.isRequired,
+        }).isRequired
+      ).isRequired,
+    })
+  ).isRequired,
   filterName: PropTypes.string.isRequired,
   setSelectedFilterIds: PropTypes.func.isRequired,
   selectedFilterIds: PropTypes.array.isRequired,
