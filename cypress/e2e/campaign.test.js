e6f8bebfb419a6958f70082f697b9a8b70cfa5e7
Alice Rühl <alice@developmentseed.org>
2021-02-05 14:47:48 +0100

    Speed up campaign test
    Avoid unit test pattern, see https://docs.cypress.io/guides/references/best-practices.html\#Creating-%E2%80%9Ctiny%E2%80%9D-tests-with-a-single-assertion

--- cypress/e2e/campaign.test.js
+++ cypress/e2e/campaign.test.js
@@ -6,173 +6,134 @@ describe("Campaign", () => {
     cy.get("[data-cy=campaigns-card]").find("big").contains("OLYMPEX").click()
   })
 
-  describe("the hero", () => {
-    it("displays the short name", () => {
-      cy.get("[data-cy=campaign-hero]").find("p").should("exist")
-    })
+  it("the hero displays titke, subtitle, and stats", () => {
+    cy.get("[data-cy=campaign-hero]").find("p").should("exist")
 
-    it("displays the long name as title", () => {
-      cy.get("[data-cy=campaign-hero]").find("h1").should("exist")
-      cy.get("[data-cy=campaign-hero-header]").should("have.length", 1)
-    })
+    cy.get("[data-cy=campaign-hero]").find("h1").should("exist")
+    cy.get("[data-cy=campaign-hero-header]").should("have.length", 1)
 
-    it("displays the focus area as subtitle", () => {
-      cy.get("[data-cy=campaign-hero]").find("p").should("exist")
-    })
-    it("displays 3 big numbers", () => {
-      cy.get("[data-cy=campaign-hero]")
-        .find("[data-cy=stats]")
-        .find("dd")
-        .should($stat => {
-          expect($stat, "3 items").to.have.length(3)
-          expect($stat.eq(0), "first item").to.contain("Deployments")
-          expect($stat.eq(1), "second item").to.contain("Collection Periods")
-          expect($stat.eq(2), "third item").to.contain("Data Products")
-        })
+    cy.get("[data-cy=campaign-hero]").find("p").should("exist")
 
-      cy.get("[data-cy=campaign-hero]")
-        .find("[data-cy=stats]")
-        .find("dt")
-        .should($stat => {
-          expect($stat, "3 items").to.have.length(3)
-          expect($stat.eq(0), "first item").to.contain("1")
-          expect($stat.eq(1), "second item").to.contain("58")
-          expect($stat.eq(2), "third item").to.contain("36")
-        })
-    })
-    it("displays a map", () => {
-      cy.get("[data-cy=mapboxgl-map]").should("exist")
-    })
-  })
-
-  describe("the inpage nav", () => {
-    it("exists", () => {
-      cy.get("main").find("nav").should("exist")
-    })
-
-    it("has 7 items", () => {
-      cy.get("main")
-        .find("nav")
-        .find("a")
-        .should($anchor => {
-          expect($anchor, "7 items").to.have.length(7)
-          expect($anchor.eq(0), "first item").to.exist
-          expect($anchor.eq(1), "second item").to.contain("Overview")
-          expect($anchor.eq(2), "third item").to.contain("Focus")
-          expect($anchor.eq(3), "fourth item").to.contain(
-            "Platforms & Instruments"
-          )
-          expect($anchor.eq(4), "fifth item").to.contain("Timeline")
-          expect($anchor.eq(5), "sixth item").to.contain("Data")
-          expect($anchor.eq(6), "seventh item").to.contain("Program Info")
-        })
-    })
-
-    it("navigates to the inpage section", () => {
-      ;["program-info", "platform", "overview", "timeline", "focus"].forEach(
-        id => {
-          cy.get(`[data-cy=${id}-inpage-link]`).click()
-
-          cy.url().should("include", id)
-
-          // TODO: figure out how to properly test the inpage scroll
-          cy.get(`[data-cy=${id}-section]`).find("h2").should("be.inViewport")
-        }
-      )
-    })
-  })
-
-  describe("the overview section", () => {
-    it("exists", () => {
-      cy.get("[data-cy=overview-section]").should("exist")
-    })
-
-    it("has a heading", () => {
-      cy.get("[data-cy=overview-section]")
-        .find("h2")
-        .should("have.text", "Overview")
-    })
-
-    it("displays a description", () => {
-      cy.get("[data-cy=description]").should("exist")
-    })
-
-    it("displays some facts", () => {
-      cy.get("[data-cy=overview-content]")
-        .find("label")
-        .should($label => {
-          expect($label, "4 items").to.have.length(4)
-
-          expect($label.eq(0), "first item").to.contain("Study dates")
-          expect($label.eq(1), "first item").to.contain("Region")
-          expect($label.eq(2), "first item").to.contain("Season of Study")
-          expect($label.eq(3), "first item").to.contain("Spatial bounds")
-        })
-    })
-
-    it("displays link list", () => {
-      cy.get("[data-cy=link-list]")
-        .find("li")
-        .should("have.length.within", 2, 5)
-    })
-
-    it("displays the DOI", () => {
-      cy.get("[data-cy=link-list]")
-        .find("[data-cy=doi-link]")
-        .should("exist")
-        .and("have.text", "http://dx.doi.org/10.5067/GPMGV/OLYMPEX/DATA101")
-    })
-  })
-
-  describe("the focus section", () => {
-    it("exists", () => {
-      cy.get("[data-cy=focus-section]").should("exist")
-    })
-
-    it("has a heading", () => {
-      cy.get("[data-cy=focus-section]").find("h2").should("have.text", "Focus")
-    })
-
-    it("displays some infos", () => {
-      cy.get("[data-cy=focus-content]").should($div => {
-        expect($div, "3 info items").to.have.length(3)
+    cy.get("[data-cy=campaign-hero]")
+      .find("[data-cy=stats]")
+      .find("dd")
+      .should($stat => {
+        expect($stat, "3 items").to.have.length(3)
+        expect($stat.eq(0), "first item").to.contain("Deployments")
+        expect($stat.eq(1), "second item").to.contain("Collection Periods")
+        expect($stat.eq(2), "third item").to.contain("Data Products")
       })
-      cy.get("[data-cy=focus-content-label]").should($label => {
-        expect($label, "3 labels").to.have.length(3)
+
+    cy.get("[data-cy=campaign-hero]")
+      .find("[data-cy=stats]")
+      .find("dt")
+      .should($stat => {
+        expect($stat, "3 items").to.have.length(3)
+        expect($stat.eq(0), "first item").to.contain("1")
+        expect($stat.eq(1), "second item").to.contain("58")
+        expect($stat.eq(2), "third item").to.contain("36")
       })
-      cy.get("[data-cy=geophysical-concept-chip]").should("exist")
-      cy.get("[data-cy=focus-phenomena-chip]").should("exist")
+
+    cy.get("[data-cy=mapboxgl-map]").should("exist")
+  })
+
+  it("the inpage nav navigates to the sections", () => {
+    cy.get("main").find("nav").should("exist")
+
+    cy.get("main")
+      .find("nav")
+      .find("a")
+      .should($anchor => {
+        expect($anchor, "7 items").to.have.length(7)
+        expect($anchor.eq(0), "first item").to.exist
+        expect($anchor.eq(1), "second item").to.contain("Overview")
+        expect($anchor.eq(2), "third item").to.contain("Focus")
+        expect($anchor.eq(3), "fourth item").to.contain(
+          "Platforms & Instruments"
+        )
+        expect($anchor.eq(4), "fifth item").to.contain("Timeline")
+        expect($anchor.eq(5), "sixth item").to.contain("Data")
+        expect($anchor.eq(6), "seventh item").to.contain("Program Info")
+      })
+    ;["program-info", "platform", "overview", "timeline", "focus"].forEach(
+      id => {
+        cy.get(`[data-cy=${id}-inpage-link]`).click()
+
+        cy.url().should("include", id)
+
+        // TODO: figure out how to properly test the inpage scroll
+        cy.get(`[data-cy=${id}-section]`).find("h2").should("be.inViewport")
+      }
+    )
+  })
+
+  it("the overview section", () => {
+    cy.get("[data-cy=overview-section]").should("exist")
+
+    cy.get("[data-cy=overview-section]")
+      .find("h2")
+      .should("have.text", "Overview")
+
+    cy.get("[data-cy=description]").should("exist")
+
+    cy.get("[data-cy=overview-content]")
+      .find("label")
+      .should($label => {
+        expect($label, "4 items").to.have.length(4)
+
+        expect($label.eq(0), "first item").to.contain("Study dates")
+        expect($label.eq(1), "first item").to.contain("Region")
+        expect($label.eq(2), "first item").to.contain("Season of Study")
+        expect($label.eq(3), "first item").to.contain("Spatial bounds")
+      })
+
+    cy.get("[data-cy=link-list]").find("li").should("have.length.within", 2, 5)
+
+    cy.get("[data-cy=link-list]")
+      .find("[data-cy=doi-link]")
+      .should("exist")
+      .and("have.text", "http://dx.doi.org/10.5067/GPMGV/OLYMPEX/DATA101")
+  })
+
+  it("the focus section displays clickable focus areas and other chips", () => {
+    cy.get("[data-cy=focus-section]").should("exist")
+
+    cy.get("[data-cy=focus-section]").find("h2").should("have.text", "Focus")
+
+    cy.get("[data-cy=focus-content]").should($div => {
+      expect($div, "3 info items").to.have.length(3)
     })
-
-    it("navigates to the focus area page", () => {
-      cy.get("[data-cy=focus-section]")
-        .find("[data-cy=focus-area]")
-        .contains("Weather")
-        .click()
-
-      cy.url().should("include", "/focus/")
-
-      cy.get("h1").should("contain", "Weather")
-
-      cy.go("back")
+    cy.get("[data-cy=focus-content-label]").should($label => {
+      expect($label, "3 labels").to.have.length(3)
     })
+    cy.get("[data-cy=geophysical-concept-chip]").should("exist")
+    cy.get("[data-cy=focus-phenomena-chip]").should("exist")
 
-    it("navigates to the campaign list with the geophysical concept as filter applied", () => {
-      cy.get("[data-cy=focus-section]")
-        .find("[data-cy=geophysical-concept]")
-        .contains("Hydrology")
-        .click()
+    cy.get("[data-cy=focus-section]")
+      .find("[data-cy=focus-area]")
+      .contains("Weather")
+      .click()
 
-      cy.url().should("include", "/explore")
+    cy.url().should("include", "/focus/")
 
-      cy.get("[data-cy=filter-chip]")
-        .should("have.length", 1)
-        .and("have.text", "geophysical: Terr Hydro")
+    cy.get("h1").should("contain", "Weather")
 
-      cy.get("[data-cy=campaigns-card]").should("have.length", 4)
+    cy.go("back")
 
-      cy.go("back")
-    })
+    cy.get("[data-cy=focus-section]")
+      .find("[data-cy=geophysical-concept]")
+      .contains("Hydrology")
+      .click()
+
+    cy.url().should("include", "/explore")
+
+    cy.get("[data-cy=filter-chip]")
+      .should("have.length", 1)
+      .and("have.text", "geophysical: Terr Hydro")
+
+    cy.get("[data-cy=campaigns-card]").should("have.length", 4)
+
+    cy.go("back")
   })
 
   describe("the timeline section", () => {
@@ -195,15 +156,11 @@ describe("Campaign", () => {
         .click()
 
       cy.get("[data-cy=milestone]").first().find("img").should("not.be.visible")
-    })
 
-    it("displays a timeline of milestones", () => {
       cy.get("[data-cy=milestone-timeline]").should("exist")
       cy.get("[data-cy=milestone-timeline]").first().find("ol").should("exist")
       cy.get("[data-cy=milestone-timeline]").first().find("li").should("exist")
-    })
 
-    it("displays a card for each li item", () => {
       cy.get("[data-cy=milestone-timeline-card]")
         .should("exist")
         .click({ multiple: true, force: true }) // needs to be forced in order to access the cards that exist outside of the window
@@ -214,55 +171,43 @@ describe("Campaign", () => {
     })
   })
 
-  describe("the platforms section", () => {
-    it("exists", () => {
-      cy.get("[data-cy=platform-section]").should("exist")
-    })
+  it("the platforms section displays platforms in a carousel accordion combo", () => {
+    cy.get("[data-cy=platform-section]").should("exist")
 
-    it("has a heading", () => {
-      cy.get("[data-cy=platform-section]")
-        .find("h2")
-        .should("have.text", "Platforms & Instruments")
-    })
+    cy.get("[data-cy=platform-section]")
+      .find("h2")
+      .should("have.text", "Platforms & Instruments")
 
-    it("displays a platform carousel", () => {
-      cy.get("[data-cy=platform-carousel]").find(".slider").should("exist")
+    cy.get("[data-cy=platform-carousel]").find(".slider").should("exist")
 
-      cy.get("[data-cy=platform-image]").should("exist")
-      cy.get("[data-cy=platform]").first().find("label").should("exist")
-      cy.get("[data-cy=platform-image-caption]").should("exist")
+    cy.get("[data-cy=platform-image]").should("exist")
+    cy.get("[data-cy=platform]").first().find("label").should("exist")
+    cy.get("[data-cy=platform-image-caption]").should("exist")
 
-      cy.get("[data-cy=platform-carousel]").find(
-        ".slider-control-centerright > button"
-      )
-    })
-    it("contains an accordion", () => {
-      cy.get("[data-cy=instrument-accordion]")
-        .should("exist")
-        .find("[data-cy=accordion-button]")
-        .first()
-        .click()
-      cy.get("[data-cy=instrument-accordion-content]")
-        .should("exist")
-        .find("img")
-      cy.get("[data-cy=instrument-accordion-content]").find(
-        "[data-cy=instrument-accordion-image-description]"
-      )
-      cy.get("[data-cy=accordion-measurements-label]").should("exist")
-      cy.get("[data-cy=accordion-link]").should("exist").first().click()
+    cy.get("[data-cy=platform-carousel]").find(
+      ".slider-control-centerright > button"
+    )
+    cy.get("[data-cy=instrument-accordion]")
+      .should("exist")
+      .find("[data-cy=accordion-button]")
+      .first()
+      .click()
+    cy.get("[data-cy=instrument-accordion-content]").should("exist").find("img")
+    cy.get("[data-cy=instrument-accordion-content]").find(
+      "[data-cy=instrument-accordion-image-description]"
+    )
+    cy.get("[data-cy=accordion-measurements-label]").should("exist")
+    cy.get("[data-cy=accordion-link]").should("exist").first().click()
 
-      cy.url().should("include", "instrument")
-      cy.go("back")
-    })
+    cy.url().should("include", "instrument")
+    cy.go("back")
 
-    it("displays some platforms", () => {
-      cy.get("[data-cy=carousel-list-text-control]")
-        .should("exist")
-        .first()
-        .should($div => {
-          expect($div, "2 or more instruments").to.have.length.gte(1)
-        })
-    })
+    cy.get("[data-cy=carousel-list-text-control]")
+      .should("exist")
+      .first()
+      .should($div => {
+        expect($div, "2 or more instruments").to.have.length.gte(1)
+      })
   })
 
   describe("the data section", () => {
@@ -271,23 +216,16 @@ describe("Campaign", () => {
       cy.get("[data-cy=campaigns-card]").find("big").contains("GCPEx").click()
     })
 
-    it("exists", () => {
+    it("displays some data products", () => {
       cy.get("[data-cy=data-section]").should("exist")
-    })
 
-    it("has a heading", () => {
       cy.get("[data-cy=data-section]")
         .find("h2")
         .should("have.text", "Data Products")
-    })
 
-    it("displays some data products", () => {
       cy.get("[data-cy=data-product]").should($div => {
         expect($div, "3 or more data products").to.have.length.gte(3)
       })
-    })
-
-    it("each data product has a label and a doi", () => {
       cy.get("[data-cy=data-product]")
         .first()
         .find("[data-cy=doi-label]")
@@ -312,34 +250,26 @@ describe("Campaign", () => {
     })
   })
 
-  describe("the program info section", () => {
-    it("exists", () => {
-      cy.get("[data-cy=program-info-section]").should("exist")
-    })
+  it("the program info section displays infos", () => {
+    cy.get("[data-cy=program-info-section]").should("exist")
 
-    it("has a heading", () => {
-      cy.get("[data-cy=program-info-section]")
-        .find("h2")
-        .should("have.text", "Program Info")
-    })
+    cy.get("[data-cy=program-info-section]")
+      .find("h2")
+      .should("have.text", "Program Info")
 
-    it("displays the logo", () => {
-      cy.get("[data-cy=campaign-logo]").should("exist")
-    })
+    cy.get("[data-cy=campaign-logo]").should("exist")
 
-    it("displays some infos", () => {
-      cy.get("[data-cy=program-info-content]").should($div => {
-        expect($div, "8 info items").to.have.length(8)
-      })
-      cy.get("[data-cy=program-info-content-label]").should($label => {
-        expect($label, "8 labels").to.have.length(8)
-      })
-      cy.get("[data-cy=program-info-content-text]").should($p => {
-        expect($p, "6 text entries").to.have.length.within(6, 8)
-      })
-      cy.get("[data-cy=program-info-content-link]").should($a => {
-        expect($a, "2 links").to.have.length.of.at.most(2)
-      })
+    cy.get("[data-cy=program-info-content]").should($div => {
+      expect($div, "8 info items").to.have.length(8)
+    })
+    cy.get("[data-cy=program-info-content-label]").should($label => {
+      expect($label, "8 labels").to.have.length(8)
+    })
+    cy.get("[data-cy=program-info-content-text]").should($p => {
+      expect($p, "6 text entries").to.have.length.within(6, 8)
+    })
+    cy.get("[data-cy=program-info-content-link]").should($a => {
+      expect($a, "2 links").to.have.length.of.at.most(2)
     })
   })
 })
