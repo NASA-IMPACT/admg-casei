62eda1c609034b780a15719382fe54cfaf5b62c2
Alice Rühl <alice@developmentseed.org>
2021-02-19 20:34:59 +0100

    Run a11y tests in isolation

--- cypress/e2e/accessibility.test.js
+++ cypress/e2e/accessibility.test.js
@@ -1,30 +1,28 @@
 /// <reference types="Cypress" />
 
 describe("Accessibility tests", () => {
-  before(() => {
+  it("Has no detectable accessibility violations on load", () => {
     cy.visit("/")
-
     cy.get("header").injectAxe()
     cy.get("main").injectAxe()
     cy.get("footer").injectAxe()
-  })
-  it("Has no detectable accessibility violations on load", () => {
+
     cy.checkA11y()
   })
 
   it("Navigates to page /explore and checks for accessibility violations", () => {
-    cy.get("nav")
-      .findByText(/Explore/i)
-      .click()
+    cy.visit("/explore")
+    cy.get("header").injectAxe()
+    cy.get("main").injectAxe()
+    cy.get("footer").injectAxe()
 
     cy.get("[data-cy=tabbar]").should("exist")
+
     cy.checkA11y()
   })
 
   it("Navigates to page for a /campaign and checks for accessibility violations", () => {
-    cy.get("nav")
-      .findByText(/Explore/i)
-      .click()
+    cy.visit("/explore")
 
     cy.get("[data-cy=tabbar]")
       .findByText(/Campaigns/i)
@@ -33,13 +31,16 @@ describe("Accessibility tests", () => {
     cy.get("[data-cy=campaigns-card]").first().click()
 
     cy.get("h1").should("exist")
+
+    cy.get("header").injectAxe()
+    cy.get("main").injectAxe()
+    cy.get("footer").injectAxe()
+
     cy.checkA11y()
   })
 
   it("Navigates to page for a /platform and checks for accessibility violations", () => {
-    cy.get("nav")
-      .findByText(/Explore/i)
-      .click()
+    cy.visit("/explore")
 
     cy.get("[data-cy=tabbar]")
       .findByText(/Platforms/i)
@@ -48,13 +49,16 @@ describe("Accessibility tests", () => {
     cy.get("[data-cy=platforms-card]").last().click()
 
     cy.get("h1").should("exist")
+
+    cy.get("header").injectAxe()
+    cy.get("main").injectAxe()
+    cy.get("footer").injectAxe()
+
     cy.checkA11y()
   })
 
   it("Navigates to page for a /instrument and checks for accessibility violations", () => {
-    cy.get("nav")
-      .findByText(/Explore/i)
-      .click()
+    cy.visit("/explore")
 
     cy.get("[data-cy=tabbar]")
       .findByText(/Instruments/i)
@@ -63,31 +67,47 @@ describe("Accessibility tests", () => {
     cy.get("[data-cy=instruments-card]").first().click()
 
     cy.get("h1").should("exist")
+
+    cy.get("header").injectAxe()
+    cy.get("main").injectAxe()
+    cy.get("footer").injectAxe()
+
     cy.checkA11y()
   })
 
   it("Navigates to page /glossary and checks for accessibility violations", () => {
-    cy.get("nav")
-      .findByText(/Glossary/i)
-      .click()
+    cy.visit("/glossary")
 
     cy.get("h1").should("exist")
+
+    cy.get("header").injectAxe()
+    cy.get("main").injectAxe()
+    cy.get("footer").injectAxe()
+
     cy.checkA11y()
   })
 
   it("Navigates to page /about and checks for accessibility violations", () => {
-    cy.get("nav").findByText(/About/i).click()
+    cy.visit("/about")
 
     cy.get("h1").should("exist")
+
+    cy.get("header").injectAxe()
+    cy.get("main").injectAxe()
+    cy.get("footer").injectAxe()
+
     cy.checkA11y()
   })
 
   it("Navigates to page /contact and checks for accessibility violations", () => {
-    cy.get("nav")
-      .findByText(/Contact/i)
-      .click()
+    cy.visit("/contact")
 
     cy.get("h1").should("exist")
+
+    cy.get("header").injectAxe()
+    cy.get("main").injectAxe()
+    cy.get("footer").injectAxe()
+
     cy.checkA11y()
   })
 })
