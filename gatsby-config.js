5a3219e32430f0d18c4f49ff2b0523c5a3d1fad7
Alice Rühl <alice@developmentseed.org>
2021-02-26 13:32:00 +0100

    Replace favicon
    unfortunately the gatsby manifest plugin doesn't support the favicon.ico format just yet, see https://github.com/gatsbyjs/gatsby/issues/8132

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,11 +37,16 @@ module.exports = {
         background_color: `#008888`,
         theme_color: `#008888`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicons/favicon-32x32.png`, // This path is relative to the root of the site.
         icons: [
           {
-            src: `/favicons/android-chrome.png`,
+            src: `/favicons/android-chrome-192x192.png`,
             sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-chrome-512x512.png`,
+            sizes: `512x512`,
             type: `image/png`,
           },
           {
