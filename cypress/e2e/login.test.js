9e14b2b63e0824491c3477df4c38e6340192795e
Alice Rühl <alice@developmentseed.org>
2021-03-22 19:21:49 +0100

    Update cy test

--- cypress/e2e/login.test.js
+++ cypress/e2e/login.test.js
@@ -1,6 +1,6 @@
 describe("Login", () => {
   before(() => {
-    cy.visit("/campaign/2a5f6d7b-2f5a-4c63-9199-62bebb98d2c4", {
+    cy.visit("/campaign/a2b7ce72-02ea-4b00-a784-ec72b7a9041a", {
       // using onLoad to overwrite fetch from cypress/support/index.json - is there a better way?
       onLoad: win => {
         win.fetch = () =>
