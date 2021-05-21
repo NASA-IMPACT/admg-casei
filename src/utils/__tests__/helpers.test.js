f620c314b2763d96b287cff602019a14b7c1161e
Rachel Wegener <rachel.e.wegener@gmail.com>
2021-05-21 08:39:12 -0500

    break tests by updating expected results to correct values

--- src/utils/__tests__/helpers.test.js
+++ src/utils/__tests__/helpers.test.js
@@ -2,9 +2,16 @@ import { convertBoundsToNSWE } from "../helpers"
 
 it("converts bounds to NSWE", () => {
   const input = [
+    // NE hemisphere test case
     "SRID=4326;POLYGON ((115 20, 130 20, 130 5, 115 5, 115 20))",
+    // NW hemisphere test case
     "SRID=4326;POLYGON ((-80 35, -70 35, -70 45, -80 45, -80 35))",
-    "SRID=4326;POLYGON ((-127 27, -60 27, -60 50, -127 50, -127 27))",
+    // SE hemisphere test case
+    "SRID=4326;POLYGON ((115 -3, 130 -3, 130 -10, 115 -10, 115 -3))",
+    // SW hemisphere test case
+    "SRID=4326;POLYGON ((-80 -22, -70 -22, -70 -15, -80 -15, -80 -22))",
+    // Cross hemisphere test case
+    "SRID=4326;POLYGON ((-10 -12, 15 -12, 15 6, -10 6, -10 -12))",
   ]
 
   const output = [
@@ -15,16 +22,28 @@ it("converts bounds to NSWE", () => {
       E: "130°E",
     },
     {
-      N: "35°N",
-      S: "45°N",
+      N: "45°N",
+      S: "35°N",
       W: "80°W",
       E: "70°W",
     },
     {
-      N: "27°N",
-      S: "50°N",
-      W: "127°W",
-      E: "60°W",
+      N: "3°S",
+      S: "10°S",
+      W: "115°E",
+      E: "130°E",
+    },
+    {
+      N: "15°S",
+      S: "22°S",
+      W: "80°W",
+      E: "70°W",
+    },
+    {
+      N: "6°N",
+      S: "12°S",
+      W: "10°W",
+      E: "15°E",
     },
   ]
 
