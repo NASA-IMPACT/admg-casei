a70a2c6a3126f62b68673bf66bdeb42975a39a8e
Edward Keeble <edward@developmentseed.org>
2022-04-08 11:15:06 -0300

    Updated comment

--- src/components/timeline/scroll-shadow.js
+++ src/components/timeline/scroll-shadow.js
@@ -77,9 +77,7 @@ export const ScrollShadow = ({ children, scrollRef }) => {
 ScrollShadow.propTypes = {
   children: PropTypes.element,
   scrollRef: PropTypes.oneOfType([
-    // Either a function
     PropTypes.func,
-    // Or the instance of a DOM native element (see the note about SSR)
     PropTypes.shape({ current: PropTypes.any }),
   ]),
 }
