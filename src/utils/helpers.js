4a9d4116eaf168ced5e8ec3d3e5e52153459945c
Edward Keeble <edward@developmentseed.org>
2022-04-28 13:12:34 -0300

    Split onlineInformation field on commas instead of newlines.

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -16,14 +16,17 @@ const trailingCommaRegex = /(^,)|(,$)/g
  */
 
 /**
- * A funtion to parse text input into an array
+ * A function to parse comma-separated text input into an array
  * @param {string} input a string to be parsed
  * @return {array} list with separated strings
  */
 export function parseTextToList(input) {
   return input
-    .split("\n")
-    .filter(x => x)
+    .split(",")
+    .filter(x => {
+      console.log(x)
+      return x
+    })
     .map(str => str.trim().replace(trailingCommaRegex, ""))
 }
 
