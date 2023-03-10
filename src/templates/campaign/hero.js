4ea9f602f628317f9fa3a036b27fb13702a4cc46
Matt Robinson <pacificrobinson@gmail.com>
2023-03-10 18:28:48 -0500

    WIP: display multiple bounding boxes in campaign view maps

--- src/templates/campaign/hero.js
+++ src/templates/campaign/hero.js
@@ -1,11 +1,12 @@
 import React, { useRef } from "react"
 import PropTypes from "prop-types"
-import { graphql, Link } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 import styled from "styled-components"
 import { GatsbyImage } from "gatsby-plugin-image"
 import turfBbox from "@turf/bbox"
+import { multiPolygon } from "@turf/helpers"
 import parse from "wellknown"
-
+import { createResolvers } from "gatsby"
 import { HeroStats } from "../../components/hero"
 import Map from "../../components/map"
 import BboxLayer from "../../components/map/bbox-layer"
@@ -46,11 +47,31 @@ const CampaignHero = ({
   countDataProducts,
   deployments,
 }) => {
+
+  // graphql query for all spatial bounds here
+  var data = useStaticQuery(allSpatialBounds)
+  // console.log(data, 'the static query data')
+
+  var listOfSpatialBounds = data.allCampaign.nodes[0].deployments.map(deployment => deployment.spatial_bounds);
+
+  console.log(listOfSpatialBounds, 'the list of spatial bounds')
+
+  // parse the array of multi spatial bounds and separate the polygon strings
+  var parsedBounds = []
+  var multiSpatialBounds = listOfSpatialBounds.forEach(element => {
+    // move the parsed elements into the parsedBounds array
+    parsedBounds.push(parse(element))
+  });
+
+  // this handles the geojson and single bounding box for the map
   const geojson = {
     type: "Feature",
     geometry: parse(bounds),
   }
   const bbox = turfBbox(geojson)
+
+  // this creates the multi polygon for the map
+  const multiPoly = multiPolygon(parsedBounds)
 
   const containerRef = useRef()
   const { height } = useContainerDimensions(containerRef)
@@ -144,6 +165,7 @@ const CampaignHero = ({
               },
               { number: countCollectionPeriods, label: "Collection Periods" },
               { number: countDataProducts, label: "Data Products" },
+              { number: 100, label: "TESTING ALL BOUNDS" }
             ]}
           />
         </div>
@@ -159,6 +181,20 @@ const CampaignHero = ({
   )
 }
 
+export const allSpatialBounds = graphql`
+  {
+    allCampaign(filter: {id: {eq: "fe4d404a-3b84-43af-a0f0-d864ec89bf7b"}}) {
+      nodes {
+        short_name
+        deployments {
+          spatial_bounds
+          short_name
+        }
+      }
+    }
+  }
+`
+
 export const heroFields = graphql`
   fragment heroFields on campaign {
     logo150h: logo {
@@ -170,6 +206,8 @@ export const heroFields = graphql`
       }
     }
     bounds: spatial_bounds
+    # COMMENT put deployments here
+
     shortname: short_name
     longname: long_name
     focus: focus_areas {
@@ -188,14 +226,14 @@ CampaignHero.propTypes = {
       childImageSharp: PropTypes.object,
     }),
   }),
-  bounds: PropTypes.string.isRequired,
+  // bounds: PropTypes.string.isRequired,
   longname: PropTypes.string,
   shortname: PropTypes.string.isRequired,
   focusListing: PropTypes.string.isRequired,
   countDeployments: PropTypes.number.isRequired,
   countCollectionPeriods: PropTypes.number.isRequired,
   countDataProducts: PropTypes.number,
-  deployments: PropTypes.array,
+  // deployments: PropTypes.array,
 }
 
 export default CampaignHero
