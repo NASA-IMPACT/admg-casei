63de726741a1c14a8d5930aa4a4d010b51159813
Matt Robinson <pacificrobinson@gmail.com>
2023-04-19 12:20:28 -0400

    added configs, dependencies, ignored playwright tests in Jest as they are run with a separate command

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+// https://jestjs.io/docs/getting-started#using-babel
+// Configure Babel to target your current version of Node by creating a babel.config.js file in the root of your project:
+
+module.exports = {
+    presets: [['@babel/preset-env', { targets: { node: 'current' } }],
+        // Jest supports TypeScript, via Babel. First, make sure you followed the instructions on using Babel above. Next, install the @babel/preset-typescript:
+        '@babel/preset-typescript',
+    ],
+};
