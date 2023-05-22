00b3d83fbb0df9c67074f281a643d8ea0546be98
Matt Robinson <pacificrobinson@gmail.com>
2023-05-22 12:26:24 -0400

    define session storage so that i can be redefined in subsequent tests

--- test/loadershim.js
+++ test/loadershim.js
@@ -2,10 +2,13 @@ global.___loader = {
   enqueue: jest.fn(),
 }
 
-global.sessionStorage = {
-  getItem: jest.fn(),
-  setItem: jest.fn(),
-}
+Object.defineProperty(global, 'sessionStorage', {
+  value: {
+    getItem: jest.fn(),
+    setItem: jest.fn(),
+  },
+});
+
 
 global.window = {
   setTimeout: jest.fn(),
