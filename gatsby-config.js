88941c1badd660e8ab80c8ce5dac8ca3558b75d1
Tammo Feldmann <31222040+Tammo-Feldmann@users.noreply.github.com>
2023-05-31 13:41:23 -0400

    Upgrade to gatsby v5

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ require("dotenv").config({
 
 module.exports = {
   pathPrefix: `/casei`,
+  trailingSlash: `always`,
   siteMetadata: {
     title: `Catalog of Archived Suborbital Earth Science Investigations`,
     shortname: `NASA | CASEI`,
@@ -65,11 +66,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-plugin-create-client-paths`,
-      options: { prefixes: [`/edit/*`] },
-    },
-
     {
       resolve: `gatsby-plugin-csp`,
       options: {
