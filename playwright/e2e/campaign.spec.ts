6e14e18c2cb236228fda377c487cf4613c255480
Tammo Feldmann <31222040+Tammo-Feldmann@users.noreply.github.com>
2023-06-26 09:43:52 -0700

    Update test to reflect new campaign page

--- playwright/e2e/campaign.spec.ts
+++ playwright/e2e/campaign.spec.ts
@@ -1,58 +1,63 @@
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test")
 import config from "../playwright.config"
 const baseUrl = config.use?.baseURL
 
-test.describe('Campaign', () => {
-    let page;
+test.describe("Campaign", () => {
+  let page
 
-    test.beforeAll(async ({ browser }) => {
-        page = await browser.newPage();
-        await page.goto(baseUrl + '/campaign/OLYMPEX');
-    });
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage()
+    await page.goto(baseUrl + "/campaign/OLYMPEX")
+  })
 
-    test('provides information on the campaign', async () => {
+  test("provides information on the campaign", async () => {
+    await expect(page.locator("[data-cy=campaign-hero] h1")).toBeVisible()
+    await expect(page.locator("[data-cy=campaign-hero-header]")).toHaveCount(1)
 
-        await expect(page.locator('[data-cy=campaign-hero] h1')).toBeVisible();
-        await expect(page.locator('[data-cy=campaign-hero-header]')).toHaveCount(1);
+    const statsItems = await page.locator(
+      "[data-cy=campaign-hero] [data-cy=stats] dd"
+    )
+    await expect(statsItems).toHaveCount(3)
+    await expect(statsItems.nth(0)).toContainText("Deployment")
+    await expect(statsItems.nth(1)).toContainText("Platforms")
+    await expect(statsItems.nth(2)).toContainText("Data Products")
 
-        const statsItems = await page.locator('[data-cy=campaign-hero] [data-cy=stats] dd');
-        await expect(statsItems).toHaveCount(3);
-        await expect(statsItems.nth(0)).toContainText('Deployment');
-        await expect(statsItems.nth(1)).toContainText('Collection Periods');
-        await expect(statsItems.nth(2)).toContainText('Data Products');
+    const statsValues = await page.locator(
+      "[data-cy=campaign-hero] [data-cy=stats] dt"
+    )
+    await expect(statsValues).toHaveCount(3)
+    await expect(statsValues.nth(0)).toContainText("1")
+    await expect(statsValues.nth(1)).toContainText("713")
+    await expect(statsValues.nth(2)).toBeVisible()
 
-        const statsValues = await page.locator('[data-cy=campaign-hero] [data-cy=stats] dt');
-        await expect(statsValues).toHaveCount(3);
-        await expect(statsValues.nth(0)).toContainText('1');
-        await expect(statsValues.nth(1)).toContainText('713');
-        await expect(statsValues.nth(2)).toBeVisible();
+    await expect(page.locator("[data-cy=mapboxgl-map]")).toBeVisible()
 
-        await expect(page.locator('[data-cy=mapboxgl-map]')).toBeVisible();
+    const inpageNavItems = await page.locator("[data-cy=inpage-nav] a")
+    await expect(inpageNavItems).toHaveCount(7)
+    await expect(inpageNavItems.nth(0)).toBeVisible()
+    await expect(inpageNavItems.nth(1)).toContainText("Overview")
+    await expect(inpageNavItems.nth(2)).toContainText("Focus")
+    await expect(inpageNavItems.nth(3)).toContainText("Platforms & Instruments")
+    await expect(inpageNavItems.nth(4)).toContainText("Timeline")
+    await expect(inpageNavItems.nth(5)).toContainText("Data")
+    await expect(inpageNavItems.nth(6)).toContainText("Program Info")
 
-        const inpageNavItems = await page.locator('[data-cy=inpage-nav] a');
-        await expect(inpageNavItems).toHaveCount(7);
-        await expect(inpageNavItems.nth(0)).toBeVisible();
-        await expect(inpageNavItems.nth(1)).toContainText('Overview');
-        await expect(inpageNavItems.nth(2)).toContainText('Focus');
-        await expect(inpageNavItems.nth(3)).toContainText('Platforms & Instruments');
-        await expect(inpageNavItems.nth(4)).toContainText('Timeline');
-        await expect(inpageNavItems.nth(5)).toContainText('Data');
-        await expect(inpageNavItems.nth(6)).toContainText('Program Info');
+    const sectionIds = [
+      "program-info",
+      "platform",
+      "overview",
+      "timeline",
+      "focus",
+    ]
 
-        const sectionIds = ['program-info', 'platform', 'overview', 'timeline', 'focus'];
+    for (const id of sectionIds) {
+      await page.locator(`[data-cy=${id}-inpage-link]`).click()
+      await expect(page.url()).toContain(id)
 
-        for (const id of sectionIds) {
-            await page.locator(`[data-cy=${id}-inpage-link]`).click();
-            await expect(page.url()).toContain(id);
+      // TODO: figure out how to properly test the inpage scroll
+      await expect(page.locator(`[data-cy=${id}-section] h2`)).toBeVisible()
+    }
+  })
 
-            // TODO: figure out how to properly test the inpage scroll
-            await expect(page.locator(`[data-cy=${id}-section] h2`)).toBeVisible();
-        }
-
-
-
-    });
-
-    // ... additional test may need to be added to cover any changes made to this format page, such as search capability
-
-});
+  // ... additional test may need to be added to cover any changes made to this format page, such as search capability
+})
