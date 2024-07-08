bb1558ec16d43ea1f634c8756d3f0b8ad45b0775
Wille Marcel <wille.yyz@gmail.com>
2024-07-08 09:50:11 -0300

    Style each moving platform with a different color (#638)
    * Style each moving platform with a different color
    
    * Use colorbrewer qualitative colors for flight paths
    
    * Update basemap, flightpath line opacity
    
    * Update platform colors test
    
    * Interpolate line paint values
    
    * Update color order
    
    * Change static platform icons and colors
    
    * Lint code
    
    * Add static icons to the map + update colors
    
    * Increase icons size
    
    Co-authored-by: Lane Goodman <lane@developmentseed.org>
    
    * Lint code
    
    * Add circle background behind icon
    
    * Swap stationary platform background and icon colors
    
    ---------
    
    Co-authored-by: LanesGood <Lane@developmentseed.org>

--- src/icons/line-icon.js
+++ src/icons/line-icon.js
@@ -9,11 +9,11 @@ const LineIcon = ({ color = "#FFF", size = "large" }) => (
     xmlns="http://www.w3.org/2000/svg"
     width={sizes[size].width}
     height={sizes[size].height}
-    viewBox="0 0 640 512"
+    viewBox="0 0 448 512"
   >
     <path
       fill={color}
-      d="M5.1 9.2C13.3-1.2 28.4-3.1 38.8 5.1l592 464c10.4 8.2 12.3 23.3 4.1 33.7s-23.3 12.3-33.7 4.1L9.2 42.9C-1.2 34.7-3.1 19.6 5.1 9.2z"
+      d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"
     />
   </svg>
 )
---
+++ src/utils/__tests__/platform-colors.test.js
@@ -0,0 +1,51 @@
+import {
+  getLineColors,
+  getStaticIcons,
+  MOVING_PLATFORMS_COLORS,
+  STATIC_PLATFORMS,
+} from "../platform-colors"
+
+describe("getLineColor", () => {
+  it("returns correct mapbox gl expression to color platforms", () => {
+    const platforms = ["DC-8", "ER-2", "GH", "Learjet"]
+    const result = [
+      "match",
+      ["get", "platform_name"],
+      "DC-8",
+      MOVING_PLATFORMS_COLORS[0],
+      "ER-2",
+      MOVING_PLATFORMS_COLORS[1],
+      "GH",
+      MOVING_PLATFORMS_COLORS[2],
+      "Learjet",
+      MOVING_PLATFORMS_COLORS[3],
+      "#1a9b8c",
+    ]
+    expect(getLineColors(platforms)).toEqual(result)
+  })
+})
+
+describe("getStaticIcons", () => {
+  it("returns correct mapbox gl expression", () => {
+    const result = [
+      "match",
+      ["get", "platform_name"],
+      STATIC_PLATFORMS[0].name,
+      STATIC_PLATFORMS[0].mapIcon,
+      STATIC_PLATFORMS[1].name,
+      STATIC_PLATFORMS[1].mapIcon,
+      STATIC_PLATFORMS[2].name,
+      STATIC_PLATFORMS[2].mapIcon,
+      STATIC_PLATFORMS[3].name,
+      STATIC_PLATFORMS[3].mapIcon,
+      STATIC_PLATFORMS[4].name,
+      STATIC_PLATFORMS[4].mapIcon,
+      STATIC_PLATFORMS[5].name,
+      STATIC_PLATFORMS[5].mapIcon,
+      STATIC_PLATFORMS[6].name,
+      STATIC_PLATFORMS[6].mapIcon,
+      "BalloonIcon",
+    ]
+    expect(getStaticIcons()).toEqual(result)
+  })
+})
