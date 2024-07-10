c06bb1816ad112c965141495f5f6dc2c0d0fba60
Wille Marcel <wille.yyz@gmail.com>
2024-07-10 18:01:14 -0300

    Fix flickering caused by the banner localStorage check delay

--- src/components/simple-banner.js
+++ src/components/simple-banner.js
@@ -7,12 +7,14 @@ import { colors } from "../theme"
 
 const SimpleBanner = () => {
   const [hasSeenWorkshopNotice, setHasSeenNotice] = useState(false)
+  const [getLocalStorage, setGetLocalStorage] = useState(false)
 
   // This runs when the page is loaded.
   useEffect(() => {
     if (localStorage.getItem("has_seen_workshop_recording_banner")) {
       setHasSeenNotice(true)
     }
+    setGetLocalStorage(true)
   }, [])
 
   const markWorkshopNoticeSeen = () => {
@@ -20,57 +22,59 @@ const SimpleBanner = () => {
     setHasSeenNotice(true)
   }
 
-  return (
-    <>
-      <div
-        css={`
-          position: relative;
-          background: ${colors[NEGATIVE].linkText};
-          color: ${colors[POSITIVE].text};
-          opacity: 0.95;
-          width: 100%;
-          justify-content: center;
-          align-items: center;
-          padding: 1rem;
-          padding-left: 2.5rem;
-          padding-right: 2.5rem;
-          z-index: 400;
-          display: ${hasSeenWorkshopNotice ? "none" : "flex"};
-        `}
-      >
-        <div>
-          <span>{`July update: Please note that session recordings from the 2024 Airborne & Field Data Workshop are now available`}</span>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href={`https://www.earthdata.nasa.gov/learn/webinars-and-tutorials/second-airborne-field-data-workshop`}
-            css={`
-              color: ${colors[POSITIVE].text};
-              font-weight: bold;
-            `}
-            data-cy={`workshop-link`}
-          >
-            {"\u00a0here"}
-          </a>
-
-          <span>!</span>
-        </div>
+  if (getLocalStorage) {
+    return (
+      <>
         <div
           css={`
-            position: absolute;
-            right: 15px;
-            top: 17px;
+            position: relative;
+            background: ${colors[NEGATIVE].linkText};
+            color: ${colors[POSITIVE].text};
+            opacity: 0.95;
+            width: 100%;
+            justify-content: center;
+            align-items: center;
+            padding: 1rem;
+            padding-left: 2.5rem;
+            padding-right: 2.5rem;
+            z-index: 400;
+            display: ${hasSeenWorkshopNotice ? "none" : "flex"};
           `}
         >
-          <IconButton
-            id="remove-filter"
-            action={markWorkshopNoticeSeen}
-            icon={<CloseIcon color={colors[POSITIVE].text} />}
-          />
+          <div>
+            <span>{`July update: Please note that session recordings from the 2024 Airborne & Field Data Workshop are now available`}</span>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={`https://www.earthdata.nasa.gov/learn/webinars-and-tutorials/second-airborne-field-data-workshop`}
+              css={`
+                color: ${colors[POSITIVE].text};
+                font-weight: bold;
+              `}
+              data-cy={`workshop-link`}
+            >
+              {"\u00a0here"}
+            </a>
+            <span>!</span>
+          </div>
+          <div
+            css={`
+              position: absolute;
+              right: 15px;
+              top: 17px;
+            `}
+          >
+            <IconButton
+              id="remove-filter"
+              action={markWorkshopNoticeSeen}
+              icon={<CloseIcon color={colors[POSITIVE].text} />}
+            />
+          </div>
         </div>
-      </div>
-    </>
-  )
+      </>
+    )
+  }
+  return <></>
 }
 
 export default SimpleBanner
