fa63493f860c5e02f5d79a9879757463911c46f4
LanesGood <Lane@developmentseed.org>
2024-08-29 14:31:23 -0400

    Fix gatsby img query

--- src/components/home/campaigns-timeline.js
+++ src/components/home/campaigns-timeline.js
@@ -32,16 +32,17 @@ export const CampaignsTimeline = ({}) => {
   const timelineData = {
     events: data.allCampaign.nodes.map(campaign => ({
       media: {
-        url: campaign.logo
-          ? campaign.logo?.gatsbyImg.childImageSharp.gatsbyImageData.images
-              .fallback.src
-          : "",
+        url:
+          campaign.logo && campaign.logo.gatsbyImg
+            ? campaign.logo?.gatsbyImg.childImageSharp.gatsbyImageData.images
+                .fallback.src
+            : "",
         link: `${data.site.pathPrefix}/campaign/${campaign.shortname}`,
-        thumbnail: campaign.logo
-          ? campaign.logo?.gatsbyImg.childImageSharp.gatsbyImageData.images
-              .fallback.src
-          : "",
-        link_target: "_self",
+        thumbnail:
+          campaign.logo && campaign.logo.gatsbyImg
+            ? campaign.logo?.gatsbyImg.childImageSharp.gatsbyImageData.images
+                .fallback.src
+            : "",
       },
       start_date: {
         month: campaign.startdate.split("-")[1],
