d5ab5c275ce7a7819fddd9393e650a550e544159
Wille Marcel <wille.yyz@gmail.com>
2025-05-06 15:07:23 -0300

    Use fallback color if there are more platforms than colors defined in the MOVING_PLATFORMS_COLORS array

--- src/utils/platform-colors.js
+++ src/utils/platform-colors.js
@@ -100,7 +100,10 @@ export const STATIC_PLATFORMS = [
 ]
 
 export const flightPathColors = platforms =>
-  platforms.map((i, index) => [i, MOVING_PLATFORMS_COLORS[index]])
+  platforms.map((i, index) => [
+    i,
+    MOVING_PLATFORMS_COLORS[index] || FALLBACK_COLOR,
+  ])
 
 const hex2rgb = hex => hex.match(/[0-9a-f]{2}/g).map(x => parseInt(x, 16))
 
